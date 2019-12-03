e2a498219775e2fe8bfa7f06fa2392989d726a64
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2019-12-03 22:55:42 +0000

    Bugfix 2 - Fixed typo "discarded" in switch.py (Switch class docstring)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,7 @@
 * v1.1.0 [2019-10-25]: First major release.
   This version is known to contain some bugs.
 
-* v1.1.1 [2019-10-25]: All relevant functions were made private in this version, to ensure encapsulation
+* v1.1.1 [2019-11-20]: All relevant functions were made private in this version, to ensure encapsulation
   (in switch.py)
+
+* v1.1.2 [2019-11-20]: Fixed typo "discarded" in docsting of class switch
--- switch.py
+++ switch.py
@@ -21,7 +21,7 @@ class Switch:
 
     self.players -- list of Player objects
     self.stock -- list of cards to draw from
-    self.discards -- list of disracded cards
+    self.discards -- list of discarded cards
     self.skip -- bool indicating that the next player is skipped
     self.draw2 -- bool indicating that the next player must draw 2 cards
     self.draw4 -- bool indicating that the next player must draw 4 cards
